268f9f9b8759987d6ba40a9ff530c96e81aff217
Jeff Ruoss <jeff@zenthree.com>
2023-03-24 11:13:28 -0700

    Clean up, organize, add to class.

---
+++ assets/src/js/PostFetcher.js
@@ -0,0 +1,43 @@
+export default class PostFetcher {
+  constructor(args = {}) {
+    this.defaults = {
+      post_type: 'post',
+      taxonomy: 'category',
+      categories: '',
+      tips_per_page: 10,
+      current_page: 1,
+      image_size: 'thumbnail',
+    };
+
+    // Merge the default values with the provided arguments
+    this.options = { ...this.defaults, ...args };
+  }
+
+  fetchPosts(args = {}) {
+    // Update the options with any new arguments provided
+    this.options = { ...this.options, ...args };
+
+    // Make an AJAX request to the PHP file
+    jQuery.ajax({
+      url: '/wp-content/plugins/ru-post-archive/ajax-tips.php',
+      type: 'GET',
+      data: this.options,
+      success: function(response) {
+        // Update the content of a DOM element with the response
+        jQuery('#tips-container').html(response);
+
+        // Add click event to pagination links
+        jQuery('.pagination-link').on('click', function(e) {
+          e.preventDefault();
+          var page = jQuery(e.currentTarget).data('page');
+          this.fetchPosts({ current_page: page });
+        }.bind(this));
+      }.bind(this),
+      error: function() {
+        console.error('An error occurred while fetching the posts.');
+      },
+    });
+  }
+
+
+}
--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,67 +1,11 @@
-import Archive from './archive';
-import Archive2 from './archive2';
+import PostFetcher from './PostFetcher';
 
-/**
- * CONFIG
- */
-// const config = {
-//   postType: 'tips',
-//   postsPerPage: 6,
-//   featuredImgSize: 'thumbnail',
-//   filterTaxonomy: 'tips_category',
-// }
+const postFetcher = new PostFetcher({
+  post_type: 'tips',
+  taxonomy: 'tips_category',
+  categories: '', // comma separated list of category slugs: 'test,another-test'
+  tips_per_page: 2,
+  image_size: 'thumbnail',
+});
 
-
-// const archive = new Archive(config);
-
-
-{
-
-  function ruGetPosts(args = {}) {
-    // Set default values for the parameters
-    const defaults = {
-      post_type: 'post',
-      taxonomy: 'category',
-      categories: '',
-      tips_per_page: 10,
-      current_page: 1,
-      image_size: 'thumbnail',
-    };
-
-    // Merge the default values with the provided arguments
-    const options = { ...defaults, ...args };
-
-    // Make an AJAX request to the PHP file
-    jQuery.ajax({
-      url: '/wp-content/plugins/ru-post-archive/ajax-tips.php',
-      type: 'GET',
-      data: options,
-      success: function (response) {
-        // Update the content of a DOM element with the response
-        jQuery('#tips-container').html(response);
-
-        // Add click event to pagination links
-        jQuery('.pagination-link').on('click', function (e) {
-          e.preventDefault();
-          var page = jQuery(this).data('page');
-          ruGetPosts({ ...options, current_page: page });
-        });
-      },
-      error: function () {
-        console.error('An error occurred while fetching the posts.');
-      },
-    });
-  }
-
-
-  ruGetPosts({
-    post_type: 'tips',
-    taxonomy: 'tips_category',
-    tips_per_page: 5,
-    image_size: 'thumbnail',
-  });
-
-
-
-}
-
+postFetcher.fetchPosts();
