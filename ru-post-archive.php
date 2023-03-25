3f7cc6346ea2a2cf2d857445ad5564683cadf8e3
Jeff Ruoss <jeff@zenthree.com>
2023-03-25 08:14:28 -0700

    Load JS and CSS only if shortcode is used.

--- ru-post-archive.php
+++ ru-post-archive.php
@@ -6,17 +6,21 @@
  */
 
 
-function ru_post_archive_enqueue_script() {
-  wp_enqueue_script( 'ru-post-archive-js', plugins_url( 'assets/dist/bundle.js', __FILE__ ), '', '1.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'ru_post_archive_enqueue_script' );
+function ru_post_archive_enqueue_scripts() {
+  global $post;
 
-function ru_post_archive_enqueue_style() {
-  wp_enqueue_style( 'ru-post-archive-css', plugins_url( 'assets/dist/bundle.css', __FILE__ ), '', '1.0', 'all' );
+  if ( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'ttt_post_archive' ) ) {
+    // Enqueue the JS file
+    wp_enqueue_script( 'ru-post-archive-js', plugins_url( 'assets/dist/bundle.js', __FILE__ ), '', '1.0', true );
+
+    // Enqueue the CSS file
+    wp_enqueue_style( 'ru-post-archive-css', plugins_url( 'assets/dist/bundle.css', __FILE__ ), '', '1.0', 'all' );
+  }
 }
-add_action( 'wp_enqueue_scripts', 'ru_post_archive_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'ru_post_archive_enqueue_scripts' );
 
 function ru_post_archive_shortcode() {
   return '<div id="ru-post-archive" class="ru-post-archive"></div>';
 }
 add_shortcode( 'ttt_post_archive', 'ru_post_archive_shortcode' );
+
